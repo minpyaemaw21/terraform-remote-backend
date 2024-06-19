8458c1d43bcaed0f417aee02a2727b7464dfd02c
minpyaemaw21 <minpyaemaw123@gmail.com>
2024-06-19 14:57:38 +0000

    outputs.tf modified

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "mp-master-s3-bucket-name" {
-    value = data.aws_s3_bucket.mp-master-bucket.arn
+    value = resource.aws_s3_bucket.mp-master-bucket.arn
 }
 
 output "mp-master-dynamodb_name" {
-    value = data.aws_dynamodb_table.terraform_locks.id
+    value = resource.aws_dynamodb_table.terraform_locks.id
 }
